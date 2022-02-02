18a5c281fb2d473ff660d47d8c136a8de55f9259
z297xu <kyrie807782352@gmail.com>
2022-02-01 22:04:27 -0500

    Upload Welcome Page V01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import ImageSlider from './components/Carousel'
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-   <div className='App'>
-    <ImageSlider />
-   </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
