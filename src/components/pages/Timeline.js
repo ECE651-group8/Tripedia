eec6ef5b3415bc438eb0dc0b0b6830b69ffa5fa8
Hongyi Shi <h235shi@uwaterloo.ca>
2022-02-22 14:55:49 -0500

    update timeline

---
+++ src/components/pages/Timeline.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import TimelineAlone from '../TimelineAlone';
+import Navbar from '../Navbar';
+
+
+
+function Timeline() {
+  return (
+
+    <div>
+
+         <Navbar/>
+         <TimelineAlone />
+         
+         
+         </div>
+  )
+}
+
+export default Timeline
---
+++ src/components/timelineBackup.js
@@ -0,0 +1,63 @@
+let timelineBackup = [
+  {
+    id: 1,
+    title: "Never get tired of the Canadian Big Falls",
+    location: "Niagara, Ontario",
+    description:
+      "Excitement bla bla bla bla bla",
+    buttonText: "See more descriptions",
+    date: "August 2021",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Charming historic town",
+    location: "Quebec City, Quebec",
+    description:
+      "Enjoying bla bla bla",
+    buttonText: "See more descriptions",
+    date: "January 2020",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "Lost in Heaven",
+    location: "Banff National Park, Alberta",
+    description:
+      "Wonderful bla bla bla bla.",
+    buttonText: "See more descriptions",
+    date: "September 2019",
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Spectacular park",
+    location: "Forillon National Park, Quebec",
+    description:
+      " can not wait bla bla bla",
+    // buttonText: "See more descriptions",
+    date: "June 2019",
+    icon: "school",
+  },
+  {
+    id: 5,
+    title: "Alberta",
+    location: "Columbia Icefield Skywalk, Alberta",
+    description:
+      "Defnitely will come back to visit.",
+    // buttonText: "College Projects",
+    date: "July 2018",
+    icon: "school",
+  },
+  {
+    id: 6,
+    title: "Alberta",
+    location: "Lake Louise, Alberta",
+    description:
+      "Perfect place for adventure Sports",
+    date: "February 2018",
+    icon: "school",
+  },
+];
+
+export default timelineBackup;
