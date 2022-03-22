a475f535e004bc6797f40d699fc3b9b5242e1f47
z297xu <kyrie807782352@gmail.com>
2022-03-22 10:27:01 -0400

    Encryption Added

--- backend/src/main/java/com/tripedia/tripediabackend/model/User.java
+++ backend/src/main/java/com/tripedia/tripediabackend/model/User.java
@@ -6,7 +6,6 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 
 @Entity
--- backend/src/main/java/com/tripedia/tripediabackend/service/UserService.java
+++ backend/src/main/java/com/tripedia/tripediabackend/service/UserService.java
@@ -3,18 +3,23 @@ package com.tripedia.tripediabackend.service;
 import com.tripedia.tripediabackend.dao.UserDao;
 import com.tripedia.tripediabackend.exceptions.UserNameEmptyException;
 import com.tripedia.tripediabackend.exceptions.UserNotExistException;
+import com.tripedia.tripediabackend.exceptions.PasswordNotExistException;
 import com.tripedia.tripediabackend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class UserService {
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
@@ -24,6 +29,11 @@ public class UserService {
         if (user.getUserName().isEmpty()){
             throw new UserNameEmptyException("User name can not be empty");
         }
+        if (user.getPassword() == null){
+            throw new PasswordNotExistException("User Password cannot be found!");
+        }
+        String encodedPswd = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPswd);
         return userDao.save(user);
     }
 
@@ -53,5 +63,7 @@ public class UserService {
         }
         return userDao.save(user);
     }
+
+    
 
 }
