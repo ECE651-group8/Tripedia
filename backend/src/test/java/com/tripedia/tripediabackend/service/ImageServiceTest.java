abb3f6735c00dbbf77466793d75c0e28deb75c21
unknown <kyrie807782352@gmail.com>
2022-04-03 16:28:00 -0400

    add comment and image test

---
+++ backend/src/test/java/com/tripedia/tripediabackend/service/ImageServiceTest.java
@@ -0,0 +1,44 @@
+package com.tripedia.tripediabackend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageServiceTest {
+
+    @Test
+    void addImage() {
+    }
+
+    @Test
+    void getAllImage() {
+    }
+
+    @Test
+    void getImageById() {
+    }
+
+    @Test
+    void delImage() {
+    }
+
+    @Test
+    void delImageById() {
+    }
+
+    @Test
+    void updateImage() {
+    }
+
+    @Test
+    void assignSpot() {
+    }
+
+    @Test
+    void assignPost() {
+    }
+
+    @Test
+    void assignUser() {
+    }
+}
