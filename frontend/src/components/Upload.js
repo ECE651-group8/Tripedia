b36b8cd93e96b07cdd4ae2d5c6ee12fd33652889
goldmjr <>
2022-04-03 17:11:38 -0400

    update

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -91,7 +91,7 @@ export default function Sidebar() {
             '/' +
             postId;
         res = await axios.get(url);
-        console.log(res);
+        console.log(res.data);
     };
 
     return (
