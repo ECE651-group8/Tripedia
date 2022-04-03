724a850851feb033f81125688a87a52937a27368
YancyLi1117 <liyang.sufe@gmail.com>
2022-04-03 10:55:00 -0400

    add timeline and detail connection

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -19,36 +19,7 @@ import ReactMarkdown from "react-markdown";
 const Item = styled(Box)(({ theme }) => ({
   ...theme.typography.body2,
 }));
-/*const data = [
 
-  {
-    content: ` But you also have to have a certain face to "share" with children.
-      There are too many children playing here, and we are too embarrassed
-      to "share" many facilities with the children. We only pass by
-      silently, <br />
-      so we strolled around for a while and then went back to the hotel to
-      rest. It may be that the sun at noon is still a little poisonous,
-      plus the excitement and lack of sleep last night, <br />
-      we modified the itinerary and decided to take a lunch break at the
-      hotel now. <br />
-      It was not until after 4 pm that we started our journey again Second
-      stop, The Distillery District. <br />
-      This is really a great place to take pictures. <br />
-      There are various photo spots nearby. Such as LOVE sign, Peace sign,
-      etc. We also met a couple of newlyweds taking wedding photos here,
-      and the beautiful atmosphere is beyond words. <br />
-      There are all kinds of small shops along the road, selling all kinds
-      of small boutiques. Here we meet a family of travelers from New
-      York. He is an art performer. <br />
-      He will have a performance in Toronto tomorrow, so he will bring his
-      parents to visit. The most beautiful thing is that the family can
-      share it together.`,
-    date: "Jan 9, 2021",
-    cost: "2000$",
-    location: "Toronto",
-    people: "4",
-  },
-];*/
 const postid = window.location.pathname.substring(8);
 async function getData() {
   const res = await fetch("http://localhost:8080/api/post/" + postid, {
