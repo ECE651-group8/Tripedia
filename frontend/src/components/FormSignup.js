a503a6b330fc94c1f558b6b9484e53d0c9ef0890
goldmjr <>
2022-04-04 09:13:35 -0400

    added select option

--- frontend/src/components/FormSignup.js
+++ frontend/src/components/FormSignup.js
@@ -3,27 +3,30 @@ import validate from './validateInfo';
 import useForm from './useForm';
 import './Form.css';
 import { useSpring, animated } from 'react-spring';
+import axios from 'axios';
 
-function LoginForm() {
+function LoginForm(props) {
     return (
         <React.Fragment>
             <label for="username">USERNAME</label>
-            <input type="text" id="username" />
+            <input type="text" id="username" onChange={props.userlogin} />
             <label for="password">PASSWORD</label>
-            <input type="text" id="password" />
+            <input type="text" id="password" onChange={props.passlogin} />
             <input type="submit" value="submit" className="submit" />
         </React.Fragment>
     );
 }
 
-function RegisterForm() {
+function RegisterForm(props) {
     return (
         <React.Fragment>
             <label for="fullname">full name</label>
-            <input type="text" id="fullname" />
+            <input type="text" id="fullname" onChange={props.userhandler} />
             <label for="email">email</label>
             <input type="text" id="email" />
-            <label for="password">password</label>
+            <label for="password" onChange={props.pwhandler}>
+                password
+            </label>
             <input type="text" id="password" />
             <label for="confirmpassword">confirm password</label>
             <input type="text" id="confirmpassword" />
@@ -57,15 +60,43 @@ const FormSignup = ({ submitForm }) => {
             : 'solid 0px transparent', //Animate bottom border of register button
     });
 
-    function registerClicked() {
+    const registerClicked = async () => {
         setRegistartionFormStatus(true);
-    }
-    function loginClicked() {
+        const res = axios.post('http://localhost:8080/api/user/add', {
+            userName: usernameLog,
+            password: passwordLog,
+        });
+        console.log(res);
+    };
+    const loginClicked = async () => {
         setRegistartionFormStatus(false);
-    }
+        const res = axios.post('http://localhost:8080/api/user/login', {
+            userName: usernameReg,
+            password: passwordReg,
+        });
+        console.log(res);
+    };
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [usernameReg, setUsernameReg] = useState('');
+    const [passwordReg, setPasswordReg] = useState('');
+    const [usernameLog, setUsernameLog] = useState('');
+    const [passwordLog, setPasswordLog] = useState('');
+
+    const usernameRegisterHandler = event => {
+        setUsernameReg(event.target.value);
+    };
+
+    const passwordRegisterHandler = event => {
+        setPasswordReg(event.target.value);
+    };
+
+    const usernameLoginHandler = event => {
+        setUsernameLog(event.target.value);
+    };
+
+    const passwordLoginHandler = event => {
+        setPasswordLog(event.target.value);
+    };
 
     return (
         <div className="form-content-right">
@@ -88,14 +119,20 @@ const FormSignup = ({ submitForm }) => {
                 </div>
                 <div className="form-group">
                     <animated.form action="" id="loginform" style={loginProps}>
-                        <LoginForm />
+                        <LoginForm
+                            userlogin={usernameLoginHandler}
+                            passlogin={passwordLoginHandler}
+                        />
                     </animated.form>
                     <animated.form
                         action=""
                         id="registerform"
                         style={registerProps}
                     >
-                        <RegisterForm />
+                        <RegisterForm
+                            userhandler={usernameRegisterHandler}
+                            pwhandler={passwordRegisterHandler}
+                        />
                     </animated.form>
                 </div>
                 <animated.div className="forgot-panel" style={loginProps}>
